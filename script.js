846ba9ac5fcacb7a57f667d22309eca1286c3c7c
CheemsMirrorX <107473249+cheemsmirrorx@users.noreply.github.com>
2026-08-23 03:05:18 +0530

    Update track title with new emoji

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const trackList = [
  {
-    title: "Track 0 - 𝑃𝑎𝑘𝑖𝑠𝑡𝑎𝑛𝑖 𝑆𝑜𝑛𝑔𝑠 𝑡ℎ𝑎𝑡 𝑦𝑜𝑢 𝑤𝑖𝑙𝑙 𝑙𝑜𝑣𝑒 » 🤍 )",
+    title: "Track 0 - 𝑃𝑎𝑘𝑖𝑠𝑡𝑎𝑛𝑖 𝑆𝑜𝑛𝑔𝑠 𝑡ℎ𝑎𝑡 𝑦𝑜𝑢 𝑤𝑖𝑙𝑙 𝑙𝑜𝑣𝑒 » ❤️‍🩹✨",
     artist: "Waris Ali",
     cover: "media/covers/file_00000000548c8208aaf74f3d76a2c94d.png",
     src: "media/songs/⊹ ࣪ ˖ᥫ᭡Pakistani playlist that You will loveᥫ᭡⊹ ࣪ ˖( TIMESTAMPS__♡) __(MP3_160K).mp3"
